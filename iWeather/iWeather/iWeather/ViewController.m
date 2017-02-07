72ba6851b65b69ab8dad66ebea8198bc06b1bd5b
Changsong Li <cli@KPLAXMB0043.local>
2017-02-07 05:44:08 -0600

    Finish GUI

--- iWeather/iWeather/iWeather/ViewController.m
+++ iWeather/iWeather/iWeather/ViewController.m
@@ -10,28 +10,22 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topConstraint;
-
 // location
 @property (strong, nonatomic) NSString *longitude;
 @property (strong, nonatomic) NSString *latitutde;
 @property (weak, nonatomic) IBOutlet UILabel *cityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentWeathDetailLabel;
 
 // current
 @property (weak, nonatomic) IBOutlet UILabel *currentTimeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *currentTempLabel;
 @property (weak, nonatomic) IBOutlet UILabel *higheAndLowLabel;
 
-// future 7 days
-@property (weak, nonatomic) IBOutlet UILabel *comingDay1Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay2Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay3Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay4Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay5Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay6Label;
-@property (weak, nonatomic) IBOutlet UILabel *comingDay7Label;
+// hour
+@property (weak, nonatomic) IBOutlet UIScrollView *hourlyScrollView;
 
-@property (strong, nonatomic) NSMutableArray *labels;
+// future 7 days
+@property (weak, nonatomic) IBOutlet UIScrollView *comingDaysScrollView;
 
 @end
 
@@ -56,55 +50,51 @@
 }
 
 - (void)updateView{
-    // init label array
-    self.labels =[[NSMutableArray alloc]init];
-    [self.labels addObject:self.comingDay1Label];
-    [self.labels addObject:self.comingDay2Label];
-    [self.labels addObject:self.comingDay3Label];
-    [self.labels addObject:self.comingDay4Label];
-    [self.labels addObject:self.comingDay5Label];
-    [self.labels addObject:self.comingDay6Label];
-    [self.labels addObject:self.comingDay7Label];
-    
-    self.cityLabel.text = @"Loading...";
-    self.currentTimeLabel.text = @"";
-    self.currentTempLabel.text = @"";
-    self.higheAndLowLabel.text = @"";
-    for(UILabel *label in self.labels){
-        label.text = @"";
-    }
     
     CGFloat width = self.view.frame.size.width;
     
     switch ((int)width) {
         case 320:
-            self.currentTimeLabel.font = [self.currentTimeLabel.font fontWithSize:13];
-            //self.currentTempLabel.font = [self.currentTempLabel.font fontWithSize:13];
-            self.higheAndLowLabel.font = [self.higheAndLowLabel.font fontWithSize:13];
-            for(UILabel *label in self.labels){
-                label.font = [label.font fontWithSize:13];
-            }
             break;
             
         default:
-            self.topConstraint.constant = 50;
             break;
     }
 }
 #pragma mark - Temperature
 
 - (void)getTemperature{
-    NSError *error;
-    NSURLResponse *response;
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.darksky.net/forecast/59189b267af121d768bcdc739d4040bc/%@,%@",self.latitutde, self.longitude]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
-    NSData *data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:url] returningResponse:&response error:&error];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      
+                                        if(data){
+                                            NSData *jsonData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+                                            dispatch_async(dispatch_get_main_queue(), ^{
+                                                [self updateCurrentLabelsWithDatas:jsonData];
+                                                [self createHourlyLabelsWithDatas:jsonData];
+                                                [self updateComming7DaysLabelsWithDatas:jsonData];                                      });
+                                        }
+                                  }];
+    [task resume];
+}
+
+static inline NSString *stringFromWeekday(int weekday){
+    static NSString *strings[] = {
+        @"Sunday",
+        @"Monday",
+        @"Tuesday",
+        @"Wednesday",
+        @"Thursday",
+        @"Friday",
+        @"Saturday",
+    };
     
-    if(data){
-        NSData *jsonData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-        [self updateCurrentLabelsWithDatas:jsonData];
-        [self updateComming7DaysLabelsWithDatas:jsonData];
-    }
+    return strings[weekday - 1];
 }
 
 - (void)updateCurrentLabelsWithDatas:(NSData *) data{
@@ -112,54 +102,169 @@
     
     // current temperature
     NSString *temp = [currently valueForKey:@"temperature"];
-    self.currentTempLabel.text = [NSString stringWithFormat:@"%@",[self doubleStringToIntTempString:temp]];
-    self.currentTempLabel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Image"]];
+    self.currentTempLabel.text = [NSString stringWithFormat:@"%.0f", [temp doubleValue]];
     [self.currentTempLabel setOpaque:NO];
     
-    // current time
+    // current weather detail
+    NSString *weatherDetail = [[[currently valueForKey:@"icon"] stringByReplacingOccurrencesOfString:@"-" withString:@" "] capitalizedString];
+    weatherDetail = [weatherDetail stringByReplacingOccurrencesOfString:@" Day" withString:@""];
+    weatherDetail = [weatherDetail stringByReplacingOccurrencesOfString:@" Night" withString:@""];
+    
+    self.currentWeathDetailLabel.text = weatherDetail;
+    
+    // current day of week
     NSString *time = [currently valueForKey:@"time"];
     NSDate *date = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
     
-    NSDateComponents *components = [[NSCalendar currentCalendar] components: NSCalendarUnitDay | NSCalendarUnitMonth  | NSCalendarUnitMinute | NSCalendarUnitHour fromDate:date];
-    NSInteger minute = [components minute];
-    NSInteger hour = [components hour];
-    NSInteger month = [components month];
-    NSInteger day = [components day];
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekday fromDate:date];
+    NSString *dayOfWeek = stringFromWeekday((int)[components weekday]);
     
-    self.currentTimeLabel.text = [NSString stringWithFormat:@"%zd/%zd  %02zd:%02zd", month, day, hour, minute];
+    self.currentTimeLabel.text = dayOfWeek;
     
     // highest and lowest temperature
     NSData *today = [[[data valueForKey:@"daily"] valueForKey:@"data"] objectAtIndex:0];
     NSString *highestTemp = [today valueForKey:@"temperatureMax"];
     NSString *lowestTemp = [today valueForKey:@"temperatureMin"];
     
-    self.higheAndLowLabel.text = [NSString stringWithFormat:@"%@ %@", [self doubleStringToIntTempString:highestTemp], [self doubleStringToIntTempString:lowestTemp]];
+    self.higheAndLowLabel.text = [NSString stringWithFormat:@"%.0f°     %.0f°", [lowestTemp doubleValue], [highestTemp doubleValue]];
     
 }
 
+- (void)createHourlyLabelsWithDatas:(NSData *) data{
+    for(int i = 0; i < 24; i++){
+        // hour detail
+        NSData *hourDetail = [[[data valueForKey:@"hourly"] valueForKey:@"data"] objectAtIndex:i];
+        NSString *time = [hourDetail valueForKey:@"time"];
+        
+        // get hour and temp string
+        NSDate *date = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+        NSDateComponents *components = [calendar components:(NSCalendarUnitHour) fromDate:date];
+        
+        long hourNum = [components hour];
+        NSString *hour = @"";
+        if(hourNum == 0){
+            hour = @"12AM";
+        }else if(hourNum == 12){
+            hour = @"12PM";
+        }else if(hourNum > 12){
+            hour = [NSString stringWithFormat:@"%ldPM", hourNum - 12];
+        }else{
+            hour = [NSString stringWithFormat:@"%ldAM", hourNum];
+        }
+        NSString *temp = [NSString stringWithFormat:@"%.0f", [[hourDetail valueForKey:@"temperature"] doubleValue]];
+        
+        //create hour labels
+        UILabel *hourLabel = [[UILabel alloc] initWithFrame:CGRectMake(10 + 55 * i , 3, 40, 30)];
+        hourLabel.backgroundColor = [UIColor clearColor];
+        hourLabel.textAlignment = NSTextAlignmentCenter;
+        hourLabel.textColor = [UIColor whiteColor];
+        hourLabel.numberOfLines = 0;
+        hourLabel.text = hour;
+        hourLabel.font = [hourLabel.font fontWithSize:12];
+        [self.hourlyScrollView addSubview:hourLabel];
+        
+        //create temperature labels
+        UILabel *tempLabel = [[UILabel alloc] initWithFrame:CGRectMake(10 + 55 * i , 50, 40, 30)];
+        tempLabel.backgroundColor = [UIColor clearColor];
+        tempLabel.textAlignment = NSTextAlignmentCenter;
+        tempLabel.textColor = [UIColor whiteColor];
+        tempLabel.numberOfLines = 0;
+        tempLabel.text = [temp stringByAppendingString:@"°"];
+        tempLabel.font = [tempLabel.font fontWithSize:12];
+        [self.hourlyScrollView addSubview:tempLabel];
+        
+        //create icon
+        NSString *iconString = [hourDetail valueForKey:@"icon"];
+        UIImageView *icon =[[UIImageView alloc] initWithFrame:CGRectMake(17 + 55 * i, 30, 25, 25)];
+        icon.image = [UIImage imageNamed:[self getIconImageNameByIcon:iconString]];
+        [self.hourlyScrollView addSubview:icon];
+    }
+    // set content size and border
+    int contentWidth = 5 + 24 * 55;
+    self.hourlyScrollView.contentSize = CGSizeMake(contentWidth, 80);
+    
+    CALayer *TopBorder = [CALayer layer];
+    TopBorder.frame = CGRectMake(0.0f, 0.0f, contentWidth, 0.4f);
+    TopBorder.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.hourlyScrollView.layer addSublayer:TopBorder];
+    
+    CALayer *botBorder = [CALayer layer];
+    botBorder.frame = CGRectMake(0.0f, self.hourlyScrollView.frame.size.height - 0.4f, contentWidth, 0.4f);
+    botBorder.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.hourlyScrollView.layer addSublayer:botBorder];
+}
+
 - (void)updateComming7DaysLabelsWithDatas:(NSData *) data{
-    for(int i = 0; i < [self.labels count]; i++){
+    for(int i = 0; i < 7; i++){
         // get all info needed
         NSData *comingDay = [[[data valueForKey:@"daily"] valueForKey:@"data"] objectAtIndex:i+1];
         NSString *time = [comingDay valueForKey:@"time"];
         NSDate *date = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
         
-        NSDateComponents *components = [[NSCalendar currentCalendar] components: NSCalendarUnitMonth | NSCalendarUnitDay fromDate:date];
+        // get day of week
+        NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekday fromDate:date];
+        NSString *dayOfWeek = stringFromWeekday((int)[components weekday]);
         
-        NSInteger day = [components day];
-        NSInteger month = [components month];
+        self.currentTimeLabel.text = dayOfWeek;
+        
+        // get lowest and highest temperature
         NSString *highestTemp = [comingDay valueForKey:@"temperatureMax"];
         NSString *lowestTemp = [comingDay valueForKey:@"temperatureMin"];
         
+        // create day of week labels
+        UILabel *dayLabel = [[UILabel alloc] initWithFrame:CGRectMake(13, 5 + 32 * i, 100, 30)];
+        dayLabel.backgroundColor = [UIColor clearColor];
+        dayLabel.textAlignment = NSTextAlignmentCenter;
+        dayLabel.textColor = [UIColor whiteColor];
+        dayLabel.numberOfLines = 0;
+        dayLabel.text = dayOfWeek;
+        dayLabel.textAlignment = NSTextAlignmentLeft;
+        dayLabel.font = [dayLabel.font fontWithSize:16];
+        [self.comingDaysScrollView addSubview:dayLabel];
         
-        // update to label
-        UILabel *comingDateLabel = [self.labels objectAtIndex:i];
-        comingDateLabel.text = [NSString stringWithFormat:@"%zd/%zd  %@-%@", month, day, [self doubleStringToIntTempString:highestTemp], [self doubleStringToIntTempString:lowestTemp]];
+        // create temperature range labels
+        UILabel *tempLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width - 113, 5 + 32 * i, 100, 30)];
+        tempLabel.backgroundColor = [UIColor clearColor];
+        tempLabel.textAlignment = NSTextAlignmentCenter;
+        tempLabel.textColor = [UIColor whiteColor];
+        tempLabel.numberOfLines = 0;
+        tempLabel.text = [NSString stringWithFormat:@"%3.0f  %3.0f", [lowestTemp doubleValue], [highestTemp doubleValue] ];
+        tempLabel.textAlignment = NSTextAlignmentRight;
+        tempLabel.font = [tempLabel.font fontWithSize:16];
+        [self.comingDaysScrollView addSubview:tempLabel];
+        
+        // set content size and border
+        int contentWidth = 5 + 32 * 7;
+        self.comingDaysScrollView.contentSize = CGSizeMake(self.comingDaysScrollView.frame.size.width, contentWidth);
+        
+        //create icon
+        NSString *iconString = [comingDay valueForKey:@"icon"];
+        UIImageView *icon =[[UIImageView alloc] initWithFrame:CGRectMake( self.view.frame.size.width / 2 - 13, 8 + 32 * i, 26, 26)];
+        icon.image = [UIImage imageNamed:[self getIconImageNameByIcon:iconString]];
+        [self.comingDaysScrollView addSubview:icon];
     }
 }
 
--(NSString *) doubleStringToIntTempString:(NSString *)temp{
-    return [NSString stringWithFormat:@"%dº",((int)[temp floatValue])];
+- (NSString *)getIconImageNameByIcon:(NSString *)icon{
+    if([icon isEqualToString:@"clear-day"])
+        return @"Sun";
+    else if([icon isEqualToString:@"clear-night"])
+        return @"Moon";
+    else if([icon isEqualToString:@"rain"])
+        return @"Rain";
+    else if([icon isEqualToString:@"snow"])
+        return @"Snow";
+    else if([icon isEqualToString:@"sleet"])
+        return @"Sleet";
+    else if([icon isEqualToString:@"wind"])
+        return @"Windy Weather";
+    else if([icon isEqualToString:@"fog"])
+        return @"Fog Day";
+    else if([icon isEqualToString:@"partly-cloudy-day"])
+        return @"Partly Cloudy Day";
+    else
+        return @"Cloud";
 }
 
 #pragma mark - Location
@@ -191,9 +296,6 @@
     // Stop Location Manager
     [locationManager stopUpdatingLocation];
     
-    // set up temperatures
-    [self getTemperature];
-    
     [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
         if(error == nil && [placemarks count] > 0) {
             placemark = [placemarks lastObject];
@@ -202,6 +304,9 @@
             NSLog(@"%@", error.debugDescription);
         }
     } ];
+    
+    // set up temperatures
+    [self getTemperature];
 }
 
 - (void)displayMessageWithTitle:(NSString *) title withMessage:(NSString *) message{
